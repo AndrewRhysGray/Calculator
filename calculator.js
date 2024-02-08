ae97a0fe0e143f36b759827eeecaa3cfb22cd157
Andrew Gray <andrew.rhysgray@hotmail.co.uk>
2024-02-08 10:55:17 +0000

    Fixed some messy event listener stuff

--- calculator.js
+++ calculator.js
@@ -1,5 +1,11 @@
-// I am challenging myself to create a calculator that can carry out addition, subtraction, multipucation and division
+// I am challenging myself to create a calculator that can carry out addition, subtraction, multiplication, and division
 
+// Buttons and text output are declared as constants
+const buttons = document.querySelectorAll("button");
+const deleteButton = document.getElementById('delete');
+const output = document.getElementById('output');
+
+// Calculator functions
 function add(n1, n2) {
     return n1 + n2;
 }
@@ -14,12 +20,26 @@ function multiply(n1, n2) {
 
 function divide(n1, n2) {
     if (n2 === 0) {
-        return "Cannot be divided"
+        return "Cannot divide by zero";
     }
     return n1 / n2;
 }
 
-console.log("Add:", add(5, 3));
-console.log("Subtract:", subtract(10, 4)); 
-console.log("Multiply:", multiply(2, 6)); 
-console.log("Divide:", divide(20, 5)); 
+// Test logs
+console.log("Addition:", add(5, 3));
+console.log("Subtraction:", subtract(10, 4));
+console.log("Multiplication:", multiply(2, 6));
+console.log("Division:", divide(20, 5));
+
+// Button event listeners
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        output.value += button.textContent;
+    });
+});
+
+// Delete button event listener
+deleteButton.addEventListener('click', () => {
+    let currentText = output.value;
+    output.value = currentText.substring(0, currentText.length - 1);
+});
